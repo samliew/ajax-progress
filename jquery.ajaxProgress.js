348de7b91534d80e09f812eddbef6876c47eaa7b
Samuel Liew <samliew@gmail.com>
2019-10-27 17:40:32 +1100

    Update jquery.ajaxProgress.js

--- jquery.ajaxProgress.js
+++ jquery.ajaxProgress.js
@@ -7,7 +7,7 @@
     jQuery('body').append(`
         <style>
         .ajax-progressbar {
-            position: absolute;
+            position: fixed;
             top: 0;
             left: 0;
             width: 100%;
